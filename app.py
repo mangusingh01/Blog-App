2a937066bb58b9660c20024b32bb99aaf256e542
Mangu Singh <84660490+mangusingh01@users.noreply.github.com>
2022-09-28 11:16:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/mangu/Desktop/projects/flask_blog-master/blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 db = SQLAlchemy(app)
 
